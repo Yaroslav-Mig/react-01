6c22f99b39eb61dece1a10f6494ead0947317f48
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2020-08-01 18:40:32 +0300

    feat: add new 2 components

---
+++ src/technologies.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Technologies = () => {
+  return (
+    <div>
+      <h1>FrontEnd</h1>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>React</li>
+        <li>NodeJS</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Technologies;
